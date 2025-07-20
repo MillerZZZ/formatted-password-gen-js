f75a1233e7d23c0056cbdc6f49bccda00225530a
Miller Zhang <yufeng.official@gmail.com>
2025-07-20 12:30:52 +0800

    Refactor: improve code using clearer structures

--- js/main.js
+++ js/main.js
@@ -23,22 +23,20 @@ const randPwd = () => {
     pwd[RandomUtils.randomBase() % 11 > 3 ? RandomUtils.randomBase() % 2 * 5 + 6 + RandomUtils.randomBase() % 4 : RandomUtils.randomBase() % 3 + 2] = RandomUtils.randomDigit();
     let cnt = 0;
     pwd.forEach((char, index) => {
-        if (char == '-')
-            cnt = 0;
-        else {
-            cnt++;
-            if (char == '*') {
-                let c = '';
-                let flag = true;
-                do {
-                    flag = false;
-                    c = RandomUtils.randomLetter();
-                    let j = 0;
-                    while (++j < cnt && !(flag = pwd[index - j] == c));
-                } while (flag);
-                pwd[index] = c;
-            }
-        }
+		if (char === '-') {
+			cnt = 0;
+			return;
+		}
+		cnt++;
+		if (char >= '0' && char <= '9')
+			return;
+		pwd[index] = (function generate() {
+			const key = RandomUtils.randomLetter();
+			for (let j = 1; j < cnt; j++)
+				if (pwd[index - j] === key)
+					return generate();
+			return key;
+		})();
     });
     return pwd.join('').slice(1);
 };
